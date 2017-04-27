acf2539660b9878d2dd426b14d474a92c5d036c1
John Codd <John Codd>
2017-04-27 15:45:14 +0100

    VR Game

---
+++ Dive VR game/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    public bool play = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (play == true)
+        {
+            if (gameObject.transform.position.x >= -10 && gameObject.transform.position.x <= 10)
+            {
+                transform.Translate(Input.acceleration.x, 0, 0);
+            }
+            if (gameObject.transform.position.x < -10)
+            {
+                gameObject.transform.position = new Vector3(-10, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+            else if (gameObject.transform.position.x > 10)
+            {
+                gameObject.transform.position = new Vector3(10, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) == true && GameObject.Find("GvrMain").transform.position.x >= -10 && GameObject.Find("GvrMain").transform.position.x <= 10)
+            {
+                transform.Translate(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow) == true && GameObject.Find("GvrMain").transform.position.x >= -10 && GameObject.Find("GvrMain").transform.position.x <= 10)
+            {
+                transform.Translate(1, 0, 0);
+            }
+            if (gameObject.name == "CameraCube")
+            {
+                if (gameObject.transform.position.z != -9)
+                {
+                    play = false;
+                    GameObject camera = GameObject.Find("GvrMain");
+                    MoveCamera cameraScript = camera.GetComponent<MoveCamera>();
+                    cameraScript.play = false;
+                }
+            }
+        }
+        if (play == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Touch anywhere
+                play = true;
+                GameObject camera = GameObject.Find("GvrMain");
+                MoveCamera cameraScript = camera.GetComponent<MoveCamera>();
+                cameraScript.play = true;
+                if (gameObject.name == "CameraCube")
+                {
+                    gameObject.transform.position = (new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, -9));
+                    Rigidbody objectRB = gameObject.GetComponent<Rigidbody>();
+                    objectRB.velocity = new Vector3(0, 0, 0);
+                }
+            }
+        }
+	}
+}
---
+++ Dive VR game/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+    bool play = true;
+
+	// Use this for initialization
+	void Start () {
+        var number = Random.Range(-10, 11);
+        gameObject.transform.position = new Vector3(number, 0.5f, gameObject.transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (play == true)
+        {
+            transform.Translate(0, 0, -0.5f);
+            if (gameObject.transform.position.z < -15)
+            {
+                var number = Random.Range(-10, 11);
+                gameObject.transform.position = new Vector3(number, 0.5f, 30);
+            }
+        }
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "GvrMain" || other.gameObject.tag == "CameraCube")
+        {
+            print("Trigger");
+            Destroy(gameObject);
+            play = false;
+            GameObject camera = GameObject.Find("GvrMain");
+            MoveCamera cameraScript = camera.GetComponent<MoveCamera>();
+            cameraScript.play = false;
+        }
+    }
+}
